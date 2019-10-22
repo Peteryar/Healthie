8f2f89a717326168283af2a7c81ee905a3f3df2e
Joshua Nwankwo <systemblakes@gmail.com>
2019-10-22 12:51:55 +0100

    commiting to branch

--- src/components/josh/josh.js
+++ src/components/josh/josh.js
@@ -4,6 +4,7 @@ class Josh extends React.Component {
     render() { 
         return ( <div>
             <h1>Hello, I'm Joshua and I'm part of the  contributors of this project</h1>
+            <h1>I this should go to branch</h1>
         </div> );
     }
 }
